fb164d1694aca68c89207d786e2db418ba92cf56
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2025-10-13 23:43:10 -0600

    added my logo

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import './Navbar.css'
-import logo from '../../assets/logo.svg'
+import akolab from '../../assets/akolab.svg'
 import underline from '../../assets/nav_underline.svg'
 import menu_open from '../../assets/menu_open.svg'
 import menu_close from '../../assets/menu_close.svg'
@@ -21,7 +21,7 @@ const Navbar = () => {
 
   return (
     <div className='navbar'>
-        <img src={logo} alt="" />
+        <img src={akolab} alt="" />
         <img src={menu_open} onClick={openMenu} alt="" className='nav-mob-open' />
         <ul ref={menuRef} className="nav-menu">
           <img src={menu_close} onClick={closeMenu} alt="" className='nav-mob-close'/>
